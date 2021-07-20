e3507d78a1a81bc543ba701a5a8c5b088d1d4626
Gustavo Allen <gustavoallen92@gmail.com>
2021-07-20 16:33:32 -0400

    finished

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { models: { User, Note }} = require('./db');
+const path = require('path');
+
+
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization
+    req.user = await User.byToken(token)
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', async(req, res, next)=> {
+  try {
+    res.send({ token: await User.authenticate(req.body)});
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/auth', requireToken, async(req, res, next)=> {
+  try {
+    res.send(req.user);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+
+app.get('/api/users/:id/notes', requireToken, async(req, res, next) => {
+  try {
+    const notes = await req.user.getNotes()
+    res.send(notes)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use((err, req, res, next)=> {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
